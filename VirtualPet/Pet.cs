ef19de7e0e7f784ecb06129c6c626f30db4471ad
JohnLaubenthal <laubenthaljohn@yahoo.com>
2018-06-03 12:10:39 -0400

    first commit

---
+++ VirtualPet/Pet.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Pet
+    {
+        //declaring fields
+        private string name;
+        private int hunger;
+        private int thirst;
+        private int boredom;
+        private int tiredness;
+        private bool thirstAlert;
+        private bool hungerAlert;
+        private bool boredomAlert;
+        private bool tiredAlert;
+
+        //declaring properties
+
+        public string Name
+        {
+            get { return this.Name; }
+            set { this.Name = value; }
+        }
+        public int Boredom
+        {
+            get { return this.Boredom; }
+            set { this.Boredom = value; }
+        }
+        public int Thirst
+        {
+            get { return this.Thirst; }
+            set { this.Thirst = value; }
+        }
+        public int Hunger
+        {
+            get { return this.Hunger; }
+            set { this.Hunger = value; }
+        }
+        public int Tiredness
+        {
+            get { return this.Tiredness; }
+            set { this.Tiredness = value; }
+        }
+
+        public bool TiredAlert
+        {
+            get { return this.TiredAlert; }
+            set { this.TiredAlert = value; }
+        }
+
+        public bool BoredomAlert
+        {
+            get { return this.BoredomAlert; }
+            set { this.BoredomAlert = value; }
+        }
+
+        public bool HungerAlert
+        {
+            get { return this.HungerAlert; }
+            set { this.HungerAlert = value; }
+        }
+
+        public bool ThirstAlert
+        {
+            get { return this.ThirstAlert; }
+            set { this.ThirstAlert = value; }
+        }
+
+        public Pet()
+        {
+            //default constructor
+        }
+
+        //loaded constructor
+        public Pet(string name, int hunger, int thirst, int boredom, int tiredness)
+        {
+            this.Name = name;
+            this.Hunger = hunger;
+            this.Thirst = thirst;
+            this.Boredom = boredom;
+            this.Tiredness = tiredness;
+        }
+
+        public void GetTick()
+        {
+            Hunger++;
+            if (Hunger > 25)
+            {
+                Hungeralert(true);
+            }
+            else
+            {
+                Hungeralert(false);
+            }
+            Thirst++;
+            if (Thirst > 5)
+            {
+                Thirstalert(true);
+            }
+            else
+            {
+                Thirstalert(false);
+            }
+            Boredom++;
+            if (Boredom > 25)
+            {
+                Boredomalert(true);
+            }
+            else
+            {
+                Boredomalert(false);
+            }
+            Tiredness++;
+            if (Tiredness > 5)
+            {
+                Tiredalert(true);
+            }
+            else
+            {
+                Tiredalert(false);
+            }
+
+        }//Tick end
+
+        public bool Thirstalert(bool Status)
+        {
+            ThirstAlert = Status;
+            return ThirstAlert;
+        }
+        public bool Hungeralert(bool Status)
+        {
+            HungerAlert = Status;
+            return HungerAlert;
+        }
+        public bool Boredomalert(bool Status)
+        {
+            BoredomAlert = Status;
+            return BoredomAlert;
+        }
+
+
+        public bool Tiredalert(bool Status)
+        {
+            TiredAlert = Status;
+            return TiredAlert;
+        }
+
+
+
+        public int SetThirst(int num)
+        {
+            this.Thirst = num;
+            return Thirst;
+        }
+
+        public int SetHunger(int num)
+        {
+            this.Hunger = num;
+            return Hunger;
+        }
+
+        public int SetTiredness(int num)
+        {
+            this.Tiredness = num;
+            return Tiredness;
+        }
+
+        public string SetName(string name)
+        {
+            this.Name = name;
+            return Name;
+        }
+        public int SetBoredom(int num)
+        {
+            this.Boredom = num;
+            return Boredom;
+        }
+
+
+    }//class end
+}//namespace end
---
+++ VirtualPet/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pet tyson = new Pet("Tyson", 25, 15, 25, 15);
+            Console.WriteLine("Welcome to Virtual Tyson Management Console");
+            Console.WriteLine("Please type Yes to manage the {0} dog?", tyson.Name);
+            string userResponse = Console.ReadLine().ToLower();
+            while (userResponse.Equals("yes"))
+            {
+                Console.WriteLine("Please select from the following to manage the {0} dog", tyson.Name);
+
+                Console.WriteLine("Select 1 to get the status of the {0} dog", tyson.Name);
+                Console.WriteLine("Select 2 to feed the {0} dog", tyson.Name);
+                Console.WriteLine("Select 3 to give water the {0} dog", tyson.Name);
+                Console.WriteLine("Select 4 to walk the {0} dog", tyson.Name);
+                Console.WriteLine("Select 5 to let the {0} dog sleep", tyson.Name);
+                int userChoice = int.Parse(Console.ReadLine());
+
+
+                switch (userChoice)
+                {
+                    case 1:
+                        tyson.GetTick();
+                        if (tyson.HungerAlert)
+                        {
+                            Console.WriteLine("The {0} is  hungry and needs to be fed.", tyson.Name);
+                            Console.WriteLine("Please enter a new food level for the {0} dog", tyson.Name);
+                            int newHunger = int.Parse(Console.ReadLine());
+                            tyson.SetHunger(newHunger);
+                        }
+
+                        if (tyson.ThirstAlert)
+                        {
+                            Console.WriteLine("The {0} is thirsty and needs water.", tyson.Name);
+                            Console.WriteLine("Please enter a new water level for the {0} dog", tyson.Name);
+                            int thirstLevel = int.Parse(Console.ReadLine());
+                            tyson.SetThirst(int.Parse(Console.ReadLine()));
+
+                        }
+                        Console.WriteLine("The {0} dog is {1} hungry and has a thirst level of {2}", tyson.Name, tyson.Hunger, tyson.Thirst);
+                        break;
+
+                    case 2:
+                        tyson.GetTick();
+                        Console.WriteLine("Enter a new boredom level.");
+                        int boredom = int.Parse(Console.ReadLine());
+                        tyson.SetBoredom(boredom);
+                        Console.WriteLine("The new boredom level of the {0} dog is {1}", tyson.Name, tyson.Boredom);
+                        break;
+                    case 3:
+                        tyson.GetTick();
+                        Console.WriteLine("Enter a new tiredness level.");
+                        int tiredness = int.Parse(Console.ReadLine());
+                        tyson.SetTiredness(tiredness);
+                        Console.WriteLine("The new tiredness level of the {0} dog is {1}", tyson.Name, tyson.Tiredness);
+                        break;
+                    default:
+                        break;
+                }
+                tyson.GetTick();
+                Console.WriteLine("Please type Yes to manage the {0} dog?", tyson.Name);
+                userResponse = Console.ReadLine().ToLower();
+            }//While end
+        }//Static Void Main End
+    }//Class end
+}//Namespace end
